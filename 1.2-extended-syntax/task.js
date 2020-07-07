c2119ef7d220c763ec4927636472fd22e05299f7
Alexandra Maltseva <etaaaaaaa@gmail.com>
2020-07-07 13:36:16 +0200

    task.js updated

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -4,9 +4,7 @@ function getResult(a,b,c){
     let d = Math.pow(b, 2) - 4 * a * c;
     let x = [];
 
-    if (d < 0) {
-        x = [''];
-    } else if (d = 0) {
+    if (d === 0) {
         x.push(-b / 2 * a);
     } else {
         x.push((-b  + Math.sqrt(d)) / (2 * a));
@@ -19,24 +17,22 @@ function getAverageMark(marks){
 
     if (marks.length === 0) {
         return 0;
-    } else {
-        if (marks.length > 5) {
-            console.log('Error: more than 5 marks!');
-            marks.splice(5);
-        }
-            averageMark = (marks.reduce((a,b) => a+b, 0)) / marks.length;
-            return averageMark;
-        }
-
+    }
+    if (marks.length > 5) {
+        console.log('Error: more than 5 marks!');
+        marks.splice(5);
+    }
+    let averageMark = (marks.reduce((a,b) => a+b, 0)) / marks.length;
+    return averageMark;
 }
 
 function askDrink(name,dateOfBirthday){
-    let date = new Date().getFullYear;
-    let age = date - dateOfBirthday;
+    let date = new Date().getYear();
+    let age = date - dateOfBirthday.getYear();
 
     if (age < 18) {
-        console.log(`Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`)
+        return `Сожалею, ${name}, но я не могу вам продать алкоголь. Зато могу предложить вам замечательный клюквенный компот!`;
     } else {
-        console.log(`Не желаете ли олд-фэшн, ${name}?`);
+        return `Не желаете ли олд-фэшн, ${name}?`;
     }
 }
