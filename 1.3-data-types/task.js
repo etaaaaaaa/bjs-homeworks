b6633643eabf0eadc3b3aaae02a5e32feab688e8
Alexandra Maltseva <etaaaaaaa@gmail.com>
2020-07-08 00:11:42 +0200

    task.js updated

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,9 +1,35 @@
+"use strict";
+
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+
+    let arr = [percent, contribution, amount, date];
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i] = Number(arr[i]);
+
+        if (Number.isNaN(arr[i])) {
+            return `Параметр ${i} содержит неправильное значение ${arr[i]}`;
+            //DONNO HOW TO WRITE THE NAME OF THE PARAMETER IN A FOR LOOP (FOOR LOOP IS NOT SUITABLE??)
+        }
+    }
+
+    let s = amount - contribution;
+    let tempVarForDate = new Date().getMonth();
+    let n = (12 - date.getMonth()) + tempVarForDate;
+//DOES THIS EVEN WORK ^
+    let p = (percent / 100) / 12;
+    let monthlyPayment = s * (p + p / (((1 + p) ** n) - 1));
+    let totalAmount = monthlyPayment * n;
+
+    return totalAmount;
 }
 
 function getGreeting(name) {
-    // код для задачи №2 писать здесь
+    if (name == 0) {
+        return `Привет, мир! Меня зовут Аноним`
+    }
+
+    return `Привет, мир! Меня зовут ${name}`
+    // код для задачи №2 писать здесь/
     // return greeting;
 }
