31d4209bad9c0c92d56fe2191ab6bb313eeb3ac1
Alexandra Maltseva <etaaaaaaa@gmail.com>
2020-09-06 23:48:59 +0200

    task.js updated

---
+++ 2.3-oop-es6/task.js
@@ -0,0 +1,134 @@
+'use strict';
+
+// Task 1
+class PrintEditionItem {
+    name;
+    releaseDate;
+    pagesCount;
+    #state = 100;
+    type = null;
+
+    constructor(name, releaseDate, pagesCount) {
+        this.name = name;
+        this.releaseDate = releaseDate;
+        this.pagesCount = pagesCount;
+    }
+
+    fix() {
+        this.setState = (this.#state * 1.5);
+    }
+
+    set setState(itemState){
+        this.#state = itemState;
+        if (itemState <= 0) {
+            this.#state = 0;
+        } else if (itemState > 100) {
+            this.#state = 100;
+        }
+    }
+
+    get getState() {
+        return this.#state;
+    }
+
+}
+
+class Magazine extends PrintEditionItem {
+    type = "magazine";
+}
+
+class Book extends PrintEditionItem {
+    type = 'book';
+    author;
+
+    constructor(author, name, releaseDate, pagesCount) {
+        super(name, releaseDate, pagesCount);
+        this.author = author;
+    }
+}
+
+class NovelBook extends Book {
+    type = 'novel';
+}
+
+class FantasticBook extends Book {
+    type = 'fantastic';
+}
+
+class DetectiveBook extends Book {
+    type = 'detective';
+}
+//
+const sherlock = new DetectiveBook("Артур Конан Дойл", "Полное собрание повестей и рассказов о Шерлоке Холмсе в одном томе", 2019, 1008);
+const picknick = new FantasticBook("Аркадий и Борис Стругацкие", "Пикник на обочине", 1972, 168);
+
+
+//Task 2
+class Library {
+    #name;
+    books = [];
+
+    constructor(name) {
+        this.#name = name;
+    }
+
+    addBooks(book) {
+        if(book.getState >= 30) {
+            this.books.push(book);
+        }
+    }
+
+    findBokBy(type, value) {
+        this._type = type;
+        this._value = value;
+        function detectType() { //checks whether the parameter we are looking for exists
+
+            for (let j = 0; j < library.books.length; j++) {
+
+                for (let i = 0; i < Object.keys(library.books[j]).length; i++) {
+                    if (type === (Object.keys(library.books[j])[i])) {
+                        return type;
+                    }
+                }
+
+                console.log('null');
+                return null;
+            }
+        }
+
+        function returnValue(a) {
+            a = detectType();
+            for (let j = 0; j < library.books.length; j++) {
+
+                if (value === library.books[j][a]){
+                    console.log(library.books[j]);
+                    return library.books[j];
+                }
+            }
+            console.log('null');
+            return null;
+        }
+        returnValue();
+    }
+
+    giveBookByName (bookName) {
+        const findBookIndex = this.books.findIndex(book => book.name === bookName);
+        let findBook = 0;
+        if (findBookIndex !== -1) {
+            findBook = this.books[findBookIndex];
+            this.books.splice(findBookIndex, 1);
+            return findBook;
+        } else {
+            console.log('null');
+            return null;
+        }
+
+    }
+
+}
+const library = new Library('newLibrary');
+library.addBooks(sherlock);
+library.addBooks(picknick);
+library.addBooks(new Magazine('Мурзилка', 1919, 60));
+
+library.findBokBy('releaseDate', 1919);
